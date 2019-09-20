07cc22214068dbe861cd0cc32b67a1894c84c805
Cheukting <cheukting.ho@gmail.com>
2019-09-20 10:00:58 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='simple_query_app',
     version='0.1',
-    py_modules=['app'],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
-        'click == 7.0',
-        #'mysql-connector == 2.2.9',
-        'pandas == 0.24.2',
-        'pytest == 5.1.2',
-        'testcontainers[mysql]'
+        'click==7.0',
+        'pandas==0.24.2',
+        'pytest==5.1.2',
+        'testcontainers[mysql]==2.5'
     ],
     entry_points='''
         [console_scripts]
-        simple_query_app=app:cli
+        simple_query_app=app.app:cli
     ''',
 )
